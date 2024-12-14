e8bccc4caf48a5686c6027ae7d9a3ae96ef5e169
Lars Thomae <lars.thomae@aol.com>
2024-12-14 12:09:05 +0100

    feat: add theme

--- jive_app/lib/provider/theme.g.dart
+++ jive_app/lib/provider/theme.g.dart
@@ -13,8 +13,7 @@ String _$darkThemeHash() => r'e418cda0cdb367ef2cc776cfdfd6fc441bfd9643';
 final darkThemeProvider = AutoDisposeProvider<ThemeData>.internal(
   darkTheme,
   name: r'darkThemeProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$darkThemeHash,
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product') ? null : _$darkThemeHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
@@ -29,8 +28,7 @@ String _$lightThemeHash() => r'a01009a66bbba3ff40caeeefd2e579a9d416458f';
 final lightThemeProvider = AutoDisposeProvider<ThemeData>.internal(
   lightTheme,
   name: r'lightThemeProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$lightThemeHash,
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product') ? null : _$lightThemeHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
@@ -42,13 +40,11 @@ String _$currentThemeModeHash() => r'e4091a9945a81f48bb8787c00e8531764eb0aeb1';
 
 /// See also [CurrentThemeMode].
 @ProviderFor(CurrentThemeMode)
-final currentThemeModeProvider =
-    AutoDisposeNotifierProvider<CurrentThemeMode, ThemeMode>.internal(
+final currentThemeModeProvider = AutoDisposeNotifierProvider<CurrentThemeMode, ThemeMode>.internal(
   CurrentThemeMode.new,
   name: r'currentThemeModeProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$currentThemeModeHash,
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$currentThemeModeHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
