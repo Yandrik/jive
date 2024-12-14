72e319ee62acd11ae3ca3df8edc2ce0528903ab1
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-12-14 13:11:33 +0100

    try more shit (im giving up)

--- jive_app/android/app/src/main/kotlin/com/github/jive_app/SpotifySDK.kt
+++ jive_app/android/app/src/main/kotlin/com/github/jive_app/SpotifySDK.kt
@@ -1,5 +1,6 @@
-package com.github.jive_app;
+package com.github.jive_app
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -7,25 +8,76 @@ import com.spotify.android.appremote.api.ConnectionParams
 import com.spotify.android.appremote.api.Connector
 import com.spotify.android.appremote.api.SpotifyAppRemote
 import com.spotify.protocol.types.Track
+import com.spotify.sdk.android.auth.AuthorizationClient
+import com.spotify.sdk.android.auth.AuthorizationRequest
+import com.spotify.sdk.android.auth.AuthorizationResponse
 
 class MainActivity : AppCompatActivity() {
 
     private val clientId = "3c21b634355844f78beebd46c8ab59c4"
-    private val redirectUri = "127.0.0.1"
     private var spotifyAppRemote: SpotifyAppRemote? = null
+    private val REQUEST_CODE = 1337
+    private val REDIRECT_URI = "http://youtube.com"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item)
+
+        val request = AuthorizationRequest.Builder(
+            clientId,
+            AuthorizationResponse.Type.TOKEN,
+            REDIRECT_URI
+        ).setScopes(arrayOf("user-read-private"))
+            .build()
+
+        AuthorizationClient.openLoginActivity(this, REQUEST_CODE, request)
     }
 
-    override fun onStart() {
-        super.onStart()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+        if (requestCode == this.REQUEST_CODE) {
+            val response = AuthorizationClient.getResponse(resultCode, intent)
+            when (response.type) {
+                AuthorizationResponse.Type.TOKEN -> {
+                    //accestoken = response.accessToken
+                    connectToSpotifyAppRemote()
+                }
+
+                AuthorizationResponse.Type.ERROR -> {
+                    Log.e("MainActivity", "Auth error: ${response.error}")
+                }
+
+                else -> {}
+            }
+        }
+    }
+
+    private fun connectToSpotifyAppRemote() {
         val connectionParams = ConnectionParams.Builder(clientId)
-            .setRedirectUri(redirectUri)
+            .setRedirectUri(REDIRECT_URI)
+            //.setAccessToken(accessToken)
             .showAuthView(true)
             .build()
 
+        SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
+            override fun onConnected(appRemote: SpotifyAppRemote) {
+                spotifyAppRemote = appRemote
+                connected()
+            }
+
+            override fun onFailure(throwable: Throwable) {
+                Log.e("MainActivity", "Failed to connect: ${throwable.message}")
+            }
+        })
+    }
+
+    /*override fun onStart() {
+        super.onStart()
+        val connectionParams = ConnectionParams.Builder(clientId)
+            .setRedirectUri(REDIRECT_URI)
+            .showAuthView(true)
+            .build()
+
+        Log.d("im so cool", "killing myself")
         SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
             override fun onConnected(appRemote: SpotifyAppRemote) {
                 spotifyAppRemote = appRemote
@@ -39,7 +91,7 @@ class MainActivity : AppCompatActivity() {
                 // Something went wrong when attempting to connect! Handle errors here
             }
         })
-    }
+    }*/
 
     private fun connected() {
         spotifyAppRemote?.let {
