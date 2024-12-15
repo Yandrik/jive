1b91a7dc923a968be29fa509b9384ab678cbcc3c
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-12-15 14:52:47 +0100

    feat: youtube search sorted by views

--- jive_app/lib/audio/youtube-integration.dart
+++ jive_app/lib/audio/youtube-integration.dart
@@ -3,7 +3,7 @@ import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 Future<VideoSearchList> searchYouTube(var searchQuery) async {
   var yt = YoutubeExplode();
-  var searchResults = await yt.search.search(searchQuery);
+  var searchResults = await yt.search.search(searchQuery, filter: SortFilters.viewCount);
   yt.close();
   logger.i("Found ${searchResults.length} results for '$searchQuery'");
   return searchResults;
