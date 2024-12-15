f0b54b10456bd7b546073b5e416cd38b787f5643
Yandrik <me@yandrik.dev>
2024-12-15 15:10:01 +0100

    fix(main): fix init riverpods

--- jive_app/lib/main.dart
+++ jive_app/lib/main.dart
@@ -34,13 +34,11 @@ class _EagerInitialization extends ConsumerWidget {
       ref.watch(prefsProvider),
     ];
 
-    print(values);
+    // print(values);
 
-    return child;
-
-    // if (values.every((value) => value is AsyncValue && value.hasValue)) {
-    //   return child;
-    // }
+    if (values.every((value) => value.hasValue)) {
+      return child;
+    }
     return const SizedBox();
   }
 }
