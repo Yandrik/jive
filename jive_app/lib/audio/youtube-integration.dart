c2396176da4cdeeb56cfe8df6a201ef3c5e2b9ce
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-12-15 15:25:20 +0100

    revert: youtube viewcount search filter

--- jive_app/lib/audio/youtube-integration.dart
+++ jive_app/lib/audio/youtube-integration.dart
@@ -3,8 +3,15 @@ import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 Future<VideoSearchList> searchYouTube(var searchQuery) async {
   var yt = YoutubeExplode();
-  var searchResults = await yt.search.search(searchQuery, filter: SortFilters.viewCount);
+  var searchResults = await yt.search.search(searchQuery, filter: TypeFilters.video);
   yt.close();
   logger.i("Found ${searchResults.length} results for '$searchQuery'");
   return searchResults;
 }
+
+/*Future<List<String>> getAutoCompleteSuggestions(String searchQuery) async {
+  var yt = YoutubeExplode();
+  
+  yt.close();
+  return suggestions;
+}*/
